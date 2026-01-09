4e5f190c4bd57f6fdf8c54358e74be66b66f6097
adambellford <22466835+adambellford@users.noreply.github.com>
2026-01-09 12:36:47 +0200

    Треуголка дальше

--- src/simple3d/Triangle.java
+++ src/simple3d/Triangle.java
@@ -10,7 +10,7 @@ package simple3d;
  *
  * @author Adam Bellford
  */
-public class Triangle {
+public final class Triangle {
     public Vec3 v0, v1, v2;
     public Vec3 normal;
     
@@ -18,6 +18,7 @@ public class Triangle {
         this.v0 = v0;
         this.v1 = v1;
         this.v2 = v2;
+        normal = new Vec3();
         computeNormal();
     }
     
@@ -25,17 +26,74 @@ public class Triangle {
         this(new Vec3(), new Vec3(), new Vec3());
     }
     
-    public void computeNormal() {
-        Vec3 edge1 = v1.copy();
-        edge1.sub(v0);
-        Vec3 edge2 = v2.copy();
-        edge2.sub(v0);
-        normal = Vec3.cross(edge1, edge2);
+    public final void computeNormal() {
+        float e1x = v1.x - v0.x;
+        float e1y = v1.y - v0.y;
+        float e1z = v1.z - v0.z;
+
+        float e2x = v2.x - v0.x;
+        float e2y = v2.y - v0.y;
+        float e2z = v2.z - v0.z;
+
+        normal.x = e1y * e2z - e1z * e2y;
+        normal.y = e1z * e2x - e1x * e2z;
+        normal.z = e1x * e2y - e1y * e2x;
+
         normal.normalize();
     }
     
+    public void set(Vec3 v0, Vec3 v1, Vec3 v2) {
+        this.v0.set(v0);
+        this.v1.set(v1);
+        this.v2.set(v2);
+        computeNormal();
+    }
+    
+    public Vec3 getCenter(Vec3 out) {
+        out.set(v0);
+        out.add(v1);
+        out.add(v2);
+        out.scale(1f / 3f);
+        return out;
+    }
+    
+    public final void computeNormalUnsafe() {
+        //
+    }
+    
+    public boolean isFacing(Vec3 viewDir) {
+        return normal.dot(viewDir) < 0f;
+    }
+    
+    public boolean isBackFacing(Vec3 cameraDir) {
+        return normal.dot(cameraDir) >= 0f;
+    }
+    
+    public boolean isDegenerate() {
+        return (
+            normal.x * normal.x + normal.y * normal.y + normal.z * normal.z
+        ) == 0f;
+    }
+    
+    public float area2() {
+        float e1x = v1.x - v0.x;
+        float e1y = v1.y - v0.y;
+        float e1z = v1.z - v0.z;
+        
+        float e2x = v2.x - v0.x;
+        float e2y = v2.y - v0.y;
+        float e2z = v2.z - v0.z;
+        
+        float cx = e1y * e2z - e1z * e2y;
+        float cy = e1z * e2x - e1x * e2z;
+        float cz = e1x * e2y - e1y * e2x;
+        
+        return 0.5f * (float)Math.sqrt(cx*cx + cy*cy + cz*cz);
+    }
+    
     public String toString() {
-        return "Triangle(" + "\n\t" + this.v0.toString()
-                + "\n\t" + this.v1.toString() + "\n\t" + this.v2.toString() + "\n)";
+        StringBuffer sb = new StringBuffer("Triangle(");
+        sb.append("\n\t").append(this.v0).append("\n\t").append(this.v1).append("\n\t").append(this.v2).append("\n)");
+        return sb.toString();
     }
 }
