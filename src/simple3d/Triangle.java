a26d999fbc13920919fd55e6ae0aa27191ec4903
adambellford <22466835+adambellford@users.noreply.github.com>
2026-01-09 12:53:29 +0200

    Треуголка дальше x2

--- src/simple3d/Triangle.java
+++ src/simple3d/Triangle.java
@@ -57,8 +57,26 @@ public final class Triangle {
         return out;
     }
     
+    public Vec3 getCenter() {
+        return new Vec3(
+            (v0.x + v1.x + v2.x) * (1f/3f),
+            (v0.y + v1.y + v2.y) * (1f/3f),
+            (v0.z + v1.z + v2.z) * (1f/3f)
+        );
+    }
+    
     public final void computeNormalUnsafe() {
-        //
+        float e1x = v1.x - v0.x;
+        float e1y = v1.y - v0.y;
+        float e1z = v1.z - v0.z;
+
+        float e2x = v2.x - v0.x;
+        float e2y = v2.y - v0.y;
+        float e2z = v2.z - v0.z;
+
+        normal.x = e1y * e2z - e1z * e2y;
+        normal.y = e1z * e2x - e1x * e2z;
+        normal.z = e1x * e2y - e1y * e2x;
     }
     
     public boolean isFacing(Vec3 viewDir) {
@@ -70,12 +88,43 @@ public final class Triangle {
     }
     
     public boolean isDegenerate() {
-        return (
-            normal.x * normal.x + normal.y * normal.y + normal.z * normal.z
-        ) == 0f;
+        // по бырому
+        return normal.lengthSq() < 1e-12f;
+    }
+    
+    public float area2Sq() {
+        float e1x = v1.x - v0.x;
+        float e1y = v1.y - v0.y;
+        float e1z = v1.z - v0.z;
+        
+        float e2x = v2.x - v0.x;
+        float e2y = v2.y - v0.y;
+        float e2z = v2.z - v0.z;
+        
+        float cx = e1y * e2z - e1z * e2y;
+        float cy = e1z * e2x - e1x * e2z;
+        float cz = e1x * e2y - e1y * e2x;
+        
+        return cx*cx + cy*cy + cz*cz;
     }
     
     public float area2() {
+        float e1x = v1.x - v0.x;
+        float e1y = v1.y - v0.y;
+        float e1z = v1.z - v0.z;
+        
+        float e2x = v2.x - v0.x;
+        float e2y = v2.y - v0.y;
+        float e2z = v2.z - v0.z;
+        
+        float cx = e1y * e2z - e1z * e2y;
+        float cy = e1z * e2x - e1x * e2z;
+        float cz = e1x * e2y - e1y * e2x;
+        
+        return (float)Math.sqrt(cx*cx + cy*cy + cz*cz);
+    }
+    
+    public float area() {
         float e1x = v1.x - v0.x;
         float e1y = v1.y - v0.y;
         float e1z = v1.z - v0.z;
